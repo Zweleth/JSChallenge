8d735f9fb5961959d44e3d7b89816b6ee739ae85
Zweleth <zwebeko@gmail.com>
2022-11-08 10:16:22 +0200

    first commit

---
+++ js/code.js
@@ -0,0 +1,6 @@
+let firstName = prompt("Please enter your first name : ");
+let lastName = prompt("Please enter your surname : ");
+let userSalary = prompt("Please enter your salary : ")
+const bonus = 500;
+let totalSalary = parseInt(userSalary) + bonus;
+alert(totalSalary)
